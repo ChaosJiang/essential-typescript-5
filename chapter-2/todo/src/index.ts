19e044750ea98a73b14ffacdf828f101895ac88b
ChrisJiang <jzcmelody@gmail.com>
2024-06-24 19:48:32 +0900

    modify todo mdoels

--- chapter-2/todo/src/index.ts
+++ chapter-2/todo/src/index.ts
@@ -1,2 +1,16 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos = [
+    new TodoItem(1, "Buy flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "call Joe", true)
+]
+
+let collection = new TodoCollection("Adam", todos);
 console.clear();
-console.log("Adam's Todo List");
+console.log(`${ collection.userName}'s Todo List`);
+
+let newId = collection.addTodo("Go for fun");
+let todoItem = collection.getTodoById(newId);
+
+todoItem.printDetails();
--- chapter-2/todo/src/todoItem.ts
+++ chapter-2/todo/src/todoItem.ts
@@ -1,11 +1,8 @@
 export class TodoItem {
-    public id:number;
-    public task: string;
-    public complete: boolean;
+    constructor(public id:number, public task: string, public complete:boolean = false) {
+    }
 
-    public constructor(id:number, task: string, complete:boolean) {
-        this.id = id;
-        this.task = task;
-        this.complete = complete;
+    printDetails(): void {
+        console.log(`${this.id}\t${this.task}\t${this.complete ? "\t(complete)": ""}\t`)
     }
 }
