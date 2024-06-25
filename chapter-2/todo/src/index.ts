a4b1103ca2a213c252308780e5abc065b841eacd
ChrisJiang <jzcmelody@gmail.com>
2024-06-25 15:43:33 +0900

    update

--- chapter-2/todo/src/index.ts
+++ chapter-2/todo/src/index.ts
@@ -8,9 +8,11 @@ let todos = [
 
 let collection = new TodoCollection("Adam", todos);
 console.clear();
-console.log(`${ collection.userName}'s Todo List`);
+console.log(`${ collection.userName}'s Todo List` + `(${collection.getItemCounts().incomplete} items to do)`);
 
-let newId = collection.addTodo("Go for fun");
-let todoItem = collection.getTodoById(newId);
+// let newId = collection.addTodo("Go for fun");
+// let todoItem = collection.getTodoById(newId);
 
-todoItem.printDetails();
+// todoItem.printDetails();
+// collection.removeComplete();
+collection.getTodoItems(true).forEach(item => item.printDetails());
